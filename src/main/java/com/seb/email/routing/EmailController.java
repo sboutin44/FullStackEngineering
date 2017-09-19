aeba994b6960834f383009f983591c71a550e53d
Sébastien Boutin <sboutin44@me.com>
2017-09-20 00:50:01 +0200

    Added a send method for SPARKPOST 	modified:   build.gradle 	modified:   src/main/java/com/seb/email/routing/EmailController.java 	modified:   src/main/java/com/seb/email/routing/EmailService.java

--- src/main/java/com/seb/email/routing/EmailController.java
+++ src/main/java/com/seb/email/routing/EmailController.java
@@ -1,6 +1,7 @@
 package com.seb.email.routing;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.json.JSONException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +10,11 @@ import javax.validation.Valid;
 @RestController
 public class EmailController {
     @RequestMapping(value = "/myEmail", method = RequestMethod.POST)
-    public MyEmail email(@RequestBody @Valid MyEmail myEmail) {
+    public MyEmail email(@RequestBody @Valid MyEmail myEmail) throws JSONException {
         EmailService emailService = new EmailService();
 
-        emailService.SendMessageViaMAILGUN(myEmail);
-
+        //emailService.SendMessageViaMAILGUN(myEmail);
+        emailService.SendMessageViaSPARKPOST(myEmail);
 
 
         return myEmail; //Send the response to the HTTP Client
--- src/main/java/com/seb/email/routing/EmailService.java
+++ src/main/java/com/seb/email/routing/EmailService.java
@@ -1,63 +1,86 @@
 package com.seb.email.routing;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.util.Base64;
-
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 
 @Service
 public class EmailService {
 
-        //mailgun
-        private final String MAILGUN_USERNAME = "api";
-        private final String MAILGUN_API_KEY = "key-13b5c4c1ac6418806aa1d263beab8456";
-        private final String MAILGUN_TOKEN = Base64.getEncoder().encodeToString((MAILGUN_USERNAME + ":" + MAILGUN_API_KEY).getBytes());
-        private final String MAILGUN_DOMAIN = "sandbox572e948ff4c242c49dfb2c627fef1b23.mailgun.org";
-        private final String MAILGUN_URL = "https://api.mailgun.net/v3/sandbox572e948ff4c242c49dfb2c627fef1b23.mailgun.org/messages";
+    //mailgun
+    private final String MAILGUN_USERNAME = "api";
+    private final String MAILGUN_API_KEY = "key-13b5c4c1ac6418806aa1d263beab8456";
+    private final String MAILGUN_TOKEN = Base64.getEncoder().encodeToString((MAILGUN_USERNAME + ":" + MAILGUN_API_KEY).getBytes());
+    private final String MAILGUN_DOMAIN = "sandbox572e948ff4c242c49dfb2c627fef1b23.mailgun.org";
+    private final String MAILGUN_URL = "https://api.mailgun.net/v3/sandbox572e948ff4c242c49dfb2c627fef1b23.mailgun.org/messages";
 
+    private final String SPARKPOST_API_KEY = "cf9a6bdc909a4c06fdd9f8cb7c1c2ae77397e63a";
+    private final String SPARKPOST_TOKEN = Base64.getEncoder().encodeToString(SPARKPOST_API_KEY.getBytes());
+    //private final String SPARKPOST_DOMAIN = ;
+    private final String SPARKPOST_URL = "https://api.sparkpost.com/api/v1/transmissions";
 
-        private final String SENDINBLUE_API_KEY = "BfkK0wPT1SODqFxN";
-        private final String SENDINBLUE_DOMAIN = "";
+    private RestTemplate restTemplate;
 
-        private RestTemplate restTemplate;
+    public void SendMessageViaMAILGUN(MyEmail email) {
+        restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
 
-        public void SendMessageViaMAILGUN (MyEmail email)
-        {
-            restTemplate = new RestTemplate();
-            RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-            restTemplateBuilder.basicAuthorization(MAILGUN_USERNAME,MAILGUN_API_KEY);
-            restTemplate = restTemplateBuilder.build();
+        httpHeaders.add(httpHeaders.AUTHORIZATION, "Basic " + MAILGUN_TOKEN);
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.add(httpHeaders.AUTHORIZATION,"Basic " + MAILGUN_TOKEN);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
 
-            //httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-            httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        /* Formatting the http body */
+        body.add("from", email.getFrom());
+        body.add("to", email.getTo());
+        body.add("subject", email.getSubject());
+        body.add("text", email.getBody());
 
-            MultiValueMap<String,String> body = new LinkedMultiValueMap<String,String>();
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, httpHeaders);
 
-            // Formatting the http body
-            body.add("from",email.getFrom());
-            body.add("to",email.getTo());
-            body.add("subject",email.getSubject());
-            body.add("text",email.getBody());
+        /* Send the request to email service provider */
+        ResponseEntity<String> response = restTemplate.postForEntity(MAILGUN_URL, request, String.class);
+        HttpStatus HttpStatus = response.getStatusCode();
+    }
 
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, httpHeaders);
+    public void SendMessageViaSPARKPOST(MyEmail email) throws JSONException {
+        restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
 
-            ResponseEntity<String> response = restTemplate.postForEntity(MAILGUN_URL, request, String.class);
-            HttpStatus HttpStatus = response.getStatusCode();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.add(httpHeaders.AUTHORIZATION, SPARKPOST_TOKEN);
 
-        }
+        /* For SPARKPOST the data are send through a Json file */
+        JSONObject jsonFileToSend = new JSONObject();
+        JSONObject options = new JSONObject();
+        JSONObject content = new JSONObject();
+        JSONObject recipients = new JSONObject();
+        JSONObject contentValue = new JSONObject();
 
+        options.put("sandbox", "true");
+        contentValue.put("from", email.getFrom());
+        contentValue.put("subject", email.getSubject());
+        contentValue.put("text", email.getBody());
+        content.put("content", contentValue);
+        recipients.put("address", email.getTo());
 
+        jsonFileToSend.put("options", options);
+        jsonFileToSend.put("content", content);
+        jsonFileToSend.put("recipients", new JSONArray(recipients));
+
+        HttpEntity<JSONObject> request = new HttpEntity<>(jsonFileToSend, httpHeaders);
+
+        // Send the request to email service provider
+        ResponseEntity<String> response = restTemplate.postForEntity(SPARKPOST_URL, request, String.class);
+        HttpStatus HttpStatus = response.getStatusCode();
+    }
 }

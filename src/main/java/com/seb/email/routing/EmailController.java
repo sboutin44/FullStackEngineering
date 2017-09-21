28e4a0f0097cf29dc08b7128f1d1465e1453844c
Sébastien Boutin <sboutin44@me.com>
2017-09-21 21:20:17 +0200

    Added method to send an Email through Elastic Email
    	modified:   src/main/java/com/seb/email/routing/EmailController.java
    	modified:   src/main/java/com/seb/email/routing/EmailService.java

--- src/main/java/com/seb/email/routing/EmailController.java
+++ src/main/java/com/seb/email/routing/EmailController.java
@@ -6,16 +6,17 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 public class EmailController {
     @RequestMapping(value = "/myEmail", method = RequestMethod.POST)
-    public MyEmail email(@RequestBody @Valid MyEmail myEmail) throws JSONException {
+    public MyEmail email(@RequestBody @Valid MyEmail myEmail) throws JSONException, IOException {
         EmailService emailService = new EmailService();
 
         //emailService.SendMessageViaMAILGUN(myEmail);
-        emailService.SendMessageViaSPARKPOST(myEmail);
-
+        //emailService.SendMessageViaSPARKPOST(myEmail);
+        emailService.SendTest(myEmail);
 
         return myEmail; //Send the response to the HTTP Client
     }
--- src/main/java/com/seb/email/routing/EmailService.java
+++ src/main/java/com/seb/email/routing/EmailService.java
@@ -11,22 +11,34 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.Base64;
 
 @Service
 public class EmailService {
 
-    //mailgun
+    /* mailgun */
     private final String MAILGUN_USERNAME = "api";
     private final String MAILGUN_API_KEY = "key-13b5c4c1ac6418806aa1d263beab8456";
     private final String MAILGUN_TOKEN = Base64.getEncoder().encodeToString((MAILGUN_USERNAME + ":" + MAILGUN_API_KEY).getBytes());
     private final String MAILGUN_DOMAIN = "sandbox572e948ff4c242c49dfb2c627fef1b23.mailgun.org";
     private final String MAILGUN_URL = "https://api.mailgun.net/v3/sandbox572e948ff4c242c49dfb2c627fef1b23.mailgun.org/messages";
 
+    /* SPARKPOST */
     private final String SPARKPOST_API_KEY = "cf9a6bdc909a4c06fdd9f8cb7c1c2ae77397e63a";
     private final String SPARKPOST_TOKEN = Base64.getEncoder().encodeToString(SPARKPOST_API_KEY.getBytes());
-    //private final String SPARKPOST_DOMAIN = ;
     private final String SPARKPOST_URL = "https://api.sparkpost.com/api/v1/transmissions";
+
+    /* ElasticEmail */
+    private final String ELASTICEMAIL_USERNAME = null;
+    private final String ELASTICEMAIL_URL = "https://api.elasticemail.com/v2/email/send";
+    private final String ELASTICEMAIL_API_KEY = "3555985e-6969-4491-be44-2873ba689636";
+    private final String ELASTICEMAIL_TOKEN = Base64.getEncoder().encodeToString(ELASTICEMAIL_API_KEY.getBytes());
+
 
     private RestTemplate restTemplate;
 
@@ -82,5 +94,54 @@ public class EmailService {
         // Send the request to email service provider
         ResponseEntity<String> response = restTemplate.postForEntity(SPARKPOST_URL, request, String.class);
         HttpStatus HttpStatus = response.getStatusCode();
+    }
+
+    public void SendMessageViaElasticEmail(MyEmail email) throws JSONException {
+        restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        String requestBody = "https://api.elasticemail.com/v2/email/send?" +
+                "apikey=3555985e-6969-4491-be44-2873ba689636" + "&" +
+                "subject=Test&from=sboutin44@me.com&" +
+                "fromName=moi&sender=&senderName=&msgFrom=&msgFromName=&replyTo=&replyToName=&to=&msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&channel=&bodyHtml=&bodyText=&charset=&charsetBodyHtml=&charsetBodyText=&encodingType=&template=&headers_firstname=firstname: myValueHere&postBack=&merge_firstname=John&timeOffSetMinutes=&poolName=My Custom Pool&isTransactional=false";
+        HttpEntity<String> request = new HttpEntity<>(requestBody, httpHeaders);
+
+    }
+
+    public String SendTest(MyEmail email) throws IOException {
+
+        String userName = "sboutin";
+        String apiKey = ELASTICEMAIL_API_KEY;
+        String from = "sboutin44@me.com";
+        String fromName = "Sebastien";
+        String subject = "Test";
+        String body = "coucou sebastien";
+        String to = "seb.boutin44@gmail.com";
+        String isTransactional = "true";
+
+        String encoding = "UTF-8";
+
+        String data = "apikey=" + URLEncoder.encode(apiKey, encoding);
+        data += "&from=" + URLEncoder.encode(from, encoding);
+        data += "&fromName=" + URLEncoder.encode(fromName, encoding);
+        data += "&subject=" + URLEncoder.encode(subject, encoding);
+        data += "&bodyHtml=" + URLEncoder.encode(body, encoding);
+        data += "&to=" + URLEncoder.encode(to, encoding);
+        data += "&isTransactional=" + URLEncoder.encode(isTransactional, encoding);
+
+        URL url = new URL(ELASTICEMAIL_URL);
+        URLConnection conn = url.openConnection();
+        conn.setDoOutput(true);
+        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+        wr.write(data);
+        wr.flush();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String result = rd.readLine();
+        wr.close();
+        rd.close();
+
+        return result;
     }
 }

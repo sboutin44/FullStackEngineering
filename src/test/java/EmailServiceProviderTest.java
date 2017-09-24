5d6959926b02b9d89f4bb9a3968a07ff01bb7607
Sébastien Boutin <sboutin44@me.com>
2017-09-24 19:52:33 +0200

    Solved a formatting issue in the email html body in tests
    	modified:   src/test/java/EmailServiceProviderTest.java

--- src/test/java/EmailServiceProviderTest.java
+++ src/test/java/EmailServiceProviderTest.java
@@ -1,8 +1,12 @@
+import com.fasterxml.jackson.core.util.JsonParserSequence;
 import com.seb.email.routing.EmailServiceProvider;
 import com.seb.email.routing.MyEmail;
 import junit.framework.TestCase;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.http.HttpStatus;
+import com.fasterxml.jackson.core.JsonParser;
 
 import java.io.FileReader;
 import java.io.UnsupportedEncodingException;
@@ -19,6 +23,7 @@ public class EmailServiceProviderTest extends TestCase {
     private EmailServiceProvider provider;
 
     protected void setUp() throws Exception {
+        super.setUp();
 
         // Get a test json from JSON_FILE_DIRECTORY
         Path pathToJson = Paths.get(JSON_FILE_DIRECTORY + JSON_FILE_TESTEMAIL);
@@ -26,19 +31,28 @@ public class EmailServiceProviderTest extends TestCase {
         JSONObject jsonObject = new JSONObject(jsonString);
         email = new MyEmail();
 
+        // html parsing
+        Document docBody = Jsoup.parse(jsonObject.getString("body")); // Convert the html as a plaintext Document object
+        String emailBody =  docBody.text();
+
         email.setTo(jsonObject.getString("to"));
         email.setToName(jsonObject.getString("to_name"));
         email.setFrom(jsonObject.getString("from"));
         email.setFromName(jsonObject.getString("from_name"));
         email.setSubject(jsonObject.getString("subject"));
-        email.setBody(jsonObject.getString("body"));
+        email.setBody(emailBody);
+
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
 
     }
 
     public void testSendMessageViaElasticEmail() throws UnsupportedEncodingException {
 
         // To identify the email in my mailbox
-        email.setSubject("Tests");
+        //email.setSubject("Tests");
 
         provider = new EmailServiceProvider(EmailServiceProvider.Providers.ELASTICEMAIL);
         assertEquals(HttpStatus.OK,provider.SendMessageViaElasticEmail(email));
@@ -47,7 +61,7 @@ public class EmailServiceProviderTest extends TestCase {
     public void testSendMessageViaMAILGUN() {
 
         // To identify the email in my mailbox
-        email.setSubject("Tests");
+        //email.setSubject("Tests");
 
         provider = new EmailServiceProvider(EmailServiceProvider.Providers.MAILGUN);
         assertEquals(HttpStatus.OK,provider.SendMessageViaMAILGUN(email));
